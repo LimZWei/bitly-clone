3585473488c2010163d58fcb98f364b74c683105
Zheng <Wei>
2015-12-29 16:44:33 +0800

    counter added

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,16 +1,25 @@
+
 get '/' do
-  erb :"static/index"
+
+	unless params[:error].nil?
+		@error = params[:error]
+	end
+  	erb :"static/index"
 end
 
 post '/urls' do
 	if params['long_url'].include?('http://localhost:9393')
-	
+		
 		redirect to '/#{params["long_url"]}'
 	end
-
+	
 	url = Url.new(long_url: params['long_url'])
-	url.shorten
-	redirect to "/url/#{url.id}"
+	if url.save
+		url.shorten
+		redirect to "/url/#{url.id}"
+	else
+		redirect "/?error=invalid url"
+	end
 end
 
 get '/url/:url_id' do
@@ -19,6 +28,14 @@ get '/url/:url_id' do
 end
 
 get '/:short_url' do
-	@original = Url.find_by(short_url: params[:short_url]).long_url
+	@url = Url.find_by(short_url: params[:short_url])
+
+	@original = @url.long_url
+	
+	@click = @url.click_count + 1
+	
+	@url.update(click_count: @click)
+	
 	redirect @original
+
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,7 @@
+require 'uri'
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+	validates :long_url, :format => { :with => /\A#{URI::regexp(['http', 'https'])}\z/}
 	def shorten
 		generator = SecureRandom.hex(6)
 		self.update(short_url: generator.to_s)
